7a2b76a93590fee38226245764f4f7bc8fa28084
Ratchanon0533 <ratchanon.tg2548@gmail.com>
2026-03-30 15:56:13 +0700

    latest

--- src/pages/dashboardorganizer.tsx
+++ src/pages/dashboardorganizer.tsx
@@ -32,6 +32,9 @@ export default function DashboardOrganizer() {
             return;
         }
         try {
+            if (!token && !organizerId){
+                console.log("ERROR")
+            }
             const parsed = JSON.parse(raw);
             let userPart: any = parsed;
             let tok: string | null = null;
